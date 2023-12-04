769f2094cb14f74f7245c46f0f6cb62e089154d3
Nithish K <nithssh@protonmail.com>
2023-12-05 00:35:00 +0530

    Refactor the D1 H1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let file = File::open("input.txt").unwrap();
-    let mut sum: u32 = 0;
-    for line in BufReader::new(file).lines() {
-        let first = line
-            .as_ref()
-            .unwrap()
-            .chars()
-            .find(|c| c.is_digit(10))
-            .map(|c| c.to_digit(10).unwrap());
+    let result1: u32 = BufReader::new(file)
+        .lines()
+        .map(|line| get_calibration_value(line.unwrap()))
+        .sum();
 
-        let last = line
-            .unwrap()
-            .chars()
-            .rfind(|c| c.to_digit(10) != None)
-            .map(|c| c.to_digit(10).unwrap());
+    println!("result1: {}", result1);
+}
 
-        sum += match (first, last) {
-            (Some(f), Some(l)) => f * 10 + l,
-            _ => 0,
-        }
+fn get_calibration_value(line: String) -> u32 {
+    let first = line
+        .chars()
+        .find(|c| c.is_digit(10))
+        .map(|c| c.to_digit(10).unwrap());
+
+    let last = line
+        .chars()
+        .rfind(|c| c.to_digit(10) != None)
+        .map(|c| c.to_digit(10).unwrap());
+
+    match (first, last) {
+        (Some(f), Some(l)) => f * 10 + l,
+        _ => 0,
     }
-    println!("sum is: {}", sum);
 }
